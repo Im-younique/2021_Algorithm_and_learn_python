ca8b2a7d2befac26d42c055661aa3f398c17e186
DongYounYim <dyyim4725@gmail.com>
2021-09-09 16:30:09 +0900

    Learning Class of python by starcraft

--- PythononUtbe/practice.py
+++ PythononUtbe/practice.py
@@ -590,3 +590,148 @@
 
 #-------------------------------------------------------------------------------
 
+# 클래스
+
+# # 마린 : 공격 유닛, 군인. 총을 쏠 수 있음
+# name = "마린" #유닛의 이름
+# hp = 40     #유닛의 체력
+# damage = 5  #유닛의 공격력
+
+# print("{0} 유닛이 생성되었습니다.".format(name))
+# print("체력 {0}, 공격력 {1}\n".format(hp, damage))
+
+# # 탱크 : 공격 유닛, 탱크. 포를 쏠 수 있는데, 일반 모드 / 시즈 모드
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 35
+
+# print("{0} 유닛이 생성되었습니다.".format(tank_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank_hp, tank_damage))
+
+# tank2_name = "탱크"
+# tank2_hp = 150
+# tank2_damage = 35
+
+# print("{0} 유닛이 생성되었습니다.".format(tank2_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank2_hp, tank2_damage))
+
+
+# def attack(name, location, damage):
+#     print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(\
+#         name, location, damage))
+
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+# attack(tank2_name, "1시", tank2_damage)
+#유닛은 엄청 많아진다.. 붕어빵 틀로 바꾸자
+
+#일반 유닛
+# class Unit:                         #부모 클래스
+#     def __init__(self, name, hp, speed):                  #__init__ 생성자 (파라미터가 일치해야함)
+#         self.name = name            #멤버변수(Attack 유닛과 겹침 -> 상속)
+#         self.hp = hp                #멤버변수(Attack 유닛과 겹침 -> 상속)
+#         self.speed = speed
+
+#     def move(self, location):
+#         print("[지상 유닛 이동]")
+#         print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(self.name, location, self.speed))
+        
+
+# #공격 유닛
+# class AttackUnit(Unit):             #(Unit 상속)자식 클래스
+#     def __init__(self, name, hp, speed, damage):                   
+#         Unit.__init__(self, name, hp, speed)                #상속 받은 것 초기화
+#         self.damage = damage        
+    
+#     def attack(self, location):         #this is method
+#         print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}] "
+#             .format(self.name, location, self.damage))      #self가 있으면 class 내부에 있는 변수 값을 쓴다.
+
+#     def damaged(self, damage):          #this is method
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은 {1} 입니다".format(self.name, self.hp))
+#         if self.hp <=0:
+#             print("{0} : 파괴되었습니다.".format(self.name))
+
+# #날 수 있는 기능을 가진 클래스
+# class Flyable:
+#     def __init__(self, flying_speed):
+#         self.flying_speed = flying_speed
+
+#     def fly(self, name, location):
+#         print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]".format(name, location, self.flying_speed))
+
+# #공중 공격 유닛 클래스
+# class FlyableAttackUnit(AttackUnit, Flyable):       #다중 상속
+#     def __init__(self, name, hp, damage, flying_speed):
+#         AttackUnit.__init__(self, name, hp, 0, damage)  # 지상 speed 0
+#         Flyable.__init__(self, flying_speed)
+
+#     def move(self, location):                       #메소드 오버라이딩 move() 재정의
+#         print("[공중 유닛 이동]")
+#         self.fly(self.name, location)
+
+# # 건물
+# class BulidingUnit(Unit):
+#     def __init__(self, name, hp, location):
+#         #Unit.__init__(self, name, hp, 0)
+#         super().__init__(name, hp, 0)           #super를 사용할때는 ()하고 self를 쓰지않는다. / 다중 상속에서는 super는 맨 처음 상속받는 것만 호출받는다.
+#         self.location = location
+
+# # 서플라이 디폿 : 건물, 1개 건물 = 8 유닛.
+# supply_depot = BulidingUnit("서플라이 디폿", 500, "7시")
+
+# def game_start():                                     #패스 활용법
+#     print("[알림] 새로운 게임을 시작합니다.")
+
+# def game_over():
+#     pass                                  #패스(아무것도 안하고 일단 넘어간다 / (함수) 완성된것 처럼 보이게함)
+
+# game_start()
+# game_over()
+
+# # 벌쳐 : 지상 유닛, 기동성이 좋음
+# vulture = AttackUnit("벌쳐", 80, 10, 20)
+
+# # 배틀크루저 : 공중 유닛, 체력도 굉장히 좋고, 공격력도 좋음
+# battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+# vulture.move("11시")
+# # battlecruiser.fly(battlecruiser.name, "9시")
+# battlecruiser.move("9시")                         #재정의 한 무브.
+
+# # 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)     #다중 상속 예.
+# valkyrie.fly(valkyrie.name, "3시") 
+
+# 드랍쉽 : 공중 유닛, 수송기. 마린/ 파이어뱃/ 탱크 등을 수송. 공격 x
+
+# #메딕 : 의무병 
+
+# #파이어뱃 : 공격 유닛, 화염방사기.
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
+
+# firebat1.damaged(25)
+# firebat1.damaged(25)
+
+
+# marine1= Unit("마린", 40, 5)                                #객체 
+# marine2= Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 35)
+
+# # 레이스 : 공중 유닛, 비행기, 클로킹 (상대방에게 보이지 않음)
+# wraith1 = Unit("레이스", 80, 5)
+# print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))     #멤버변수를 외부에서 쓸 수 있다.
+
+# # 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+# wraith2 = Unit("빼앗은 레이스", 80, 5)
+# wraith2.clocking = True                 #객체에 추가로 변수를 외부에서 만들어서 사용가능
+
+# if wraith2.clocking == True:
+#     print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
+
+#----------------------------------------------------------------------------
+
+

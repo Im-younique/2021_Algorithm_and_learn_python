2cc21cee66b65d29d7c7dfebc9b3e4655e6c0db1
DongYounYim <dyyim4725@gmail.com>
2021-09-13 15:44:59 +0900

    Revise the comment

--- Lecture/permutation.py
+++ Lecture/permutation.py
@@ -1,4 +1,5 @@
 #이해가 잘 안됩니다 교슈님...
+#본인의 코드로 만들어서 풀어보기.
 
 def perm(input_list):       #n개의 정수를 나열하는 모든 경우의 수
     length = len(input_list)
@@ -7,11 +8,11 @@ def perm(input_list):       #n개의 정수를 나열하는 모든 경우의 수
     else:
         result = []
         for i in input_list:
-            temp = input_list.copy()        #임시로 한개를 카피한다?
-            temp.remove(i)                  #임시에서 뭘 제거한다고?
-            for j in perm(temp):            #나머지는 permutation 한 결과?
+            temp = input_list.copy()        #임시로 한개 선택해서 한 자리에
+            temp.remove(i)                  #임시로 선택한거 제거
+            for j in perm(temp):            #선택한거 제외한 나머지는 permutation
                 j.insert(0, i)              
-                if j not in result:         #원하는 결과를 하나씩 추가
+                if j not in result:         #결과를 하나씩 추가
                     result.append(j)
     return result               #원하는 결과를 return해서 종료
 

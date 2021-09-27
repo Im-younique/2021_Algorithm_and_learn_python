090373979f81c9841db95941cb14ce1c7ed13c63
DongYounYim <dyyim4725@gmail.com>
2021-09-27 13:38:47 +0900

    Add a comment and understand the lecture

--- Lecture/change.py
+++ Lecture/change.py
@@ -1,28 +1,28 @@
 #1, 5, 10, 25 cents 동전이 있을 때, 주어진 금액을 최소 개수의 동전으로 잔돈을 바꾸시오
 
-def make_change(coins, target) :
-    min_coins = target  #최소 동전 값이 target값이라 가정 1원으로 우다다다
+def make_change(coin_list, target) : #찾는 목표 값 target #잔돈의 종류 coin_list
+    min_coins = target  #target의 값이 1원일 때
 
-    if target in coins: #target의 숫자가 coins안에 있을때 즉, target이 1,5,10,25일때
+    if target in coin_list: #target의 숫자가 coins안에 있을때 즉, target이 1,5,10,25일때
         return 1
 
     else:
-        coins_list = list()
-        for coin in coins :
-            if coin <= target:
-                coins_list.append(coin)
-        for coin in coins_list:
-            num_coins = 1 + make_change(coins, target - coin)
-            if num_coins < min_coins:
+        coins = list()
+        for coin in coin_list:
+            if coin <= target:  #찾는 값보다 작거나 같은 잔돈종류
+                coins.append(coin)
+        for coin in coins:
+            num_coins = 1 + make_change(coin_list, target - coin)   #재귀로 모든 경우 찾음
+            if num_coins < min_coins:   #최솟값을 구하는 부분
                 min_coins = num_coins
     return min_coins
             
 
 
 def main():
-    input()
-    coins = list(map(int, input().split(" "))) #코인의 종류 list {1, 5, 10 ,25}
-    target =  int(input())  #잔돈
+    input()   #(사용)동전의 갯수
+    coins = list(map(int, input().split(" "))) #잔돈의 종류 list {1, 5, 10 ,25}
+    target =  int(input())  #찾으려는 값 ex 30 / 32
     print(make_change(coins, target))
 
 if __name__ == '__main__':

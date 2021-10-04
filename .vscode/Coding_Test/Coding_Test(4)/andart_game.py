9536d78e64fd07d86102c13103363e38144aa426
DongYounYim <dyyim4725@gmail.com>
2021-10-05 00:09:55 +0900

    Add comment

--- .vscode/Coding_Test/Coding_Test(4)/andart_game.py
+++ .vscode/Coding_Test/Coding_Test(4)/andart_game.py
@@ -6,7 +6,7 @@ def dart_score(s):
     score_list = p.findall(s)
 
     result = []
-    has_star = False    #이전에 *가 있었는가?
+    has_star = False    #이전에 *가 있었는가를 판단하는 has_star
     for i, score in enumerate(score_list):
         #score_list의 인덱스의 번호와 컬렉션의 원소를 tuple형태로 반환
         #score_list[0] = ex)1S*
@@ -19,29 +19,29 @@ def dart_score(s):
         elif bonus == 'D' : bonus = 2
         elif bonus == 'T' : bonus = 3
         if option == '*':
-            if i == 2:
+            if i == 2:  #마지막 스타는 본인 것만 두배
                 result.append(int(point)**bonus*2)
             else:
-                if has_star:
+                if has_star:    #본인이 스타를 가지고 이전에 스타가 있다면 4배!
                     result.append(int(point)**bonus*4)
                     has_star = True
-                else:
+                else:           #본인이 스타를 가졌다
                     result.append(int(point)**bonus*2)
-                    has_star = True
+                    has_star = True #다음것을 비교할 때 전에 star있다고 나타냄
         elif option == '#':
             if i == 0:
                 result.append(int(point)**bonus*(-1))
             else:
-                if has_star:
+                if has_star:    #이전에 스타가 있으면 -2배
                     result.append(int(point)**bonus*(-2))
-                    has_star = False
+                    has_star = False    #이번에는 #이므로 star는 없다
                 else:
                     result.append(int(point)**bonus*(-1))
         else:
             if i == 0:
                 result.append(int(point)**bonus)
             else:
-                if has_star:
+                if has_star:    #이전에 스타가 있으면 2배
                     result.append(int(point)**bonus*2)
                     has_star = False
                 else:
--- .vscode/Coding_Test/Coding_Test(4)/palindrome.py
+++ .vscode/Coding_Test/Coding_Test(4)/palindrome.py
@@ -1,4 +1,4 @@
-def palindrome(s) :
+def palindrome(s) :     #문자열을 뒤집어서 같은지 확인
     if s==s[::-1]:
         return True
     else:
@@ -12,9 +12,9 @@ result = ""
 
 for i in range(0, len(str)):        #i는 0부터 str의 길이까지
     for j in range(i+1, len(str)):  #j는 i+1부터 str의 길이까지 
-        substr = str[i:j]          #i부터 j직전까지  
-        if palindrome(substr):
-            if(len(substr) > longest):
+        substr = str[i:j]           #i부터 j직전까지  
+        if palindrome(substr):      #substr이 palindrome인지 확인
+            if(len(substr) > longest):  #가장긴 palindrome을 찾기위해 longest와 비교
                 longest = len(substr)
                 result = substr
 

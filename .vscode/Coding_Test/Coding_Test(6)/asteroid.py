65e2d1120ba161568ee5607d85b4f42f4359ed8a
DongYounYim <dyyim4725@gmail.com>
2021-10-13 23:40:48 +0900

    Add comment on Coding_Test(6)

--- .vscode/Coding_Test/Coding_Test(6)/asteroid.py
+++ .vscode/Coding_Test/Coding_Test(6)/asteroid.py
@@ -1,27 +1,27 @@
 
 def weigth(asteroid_list):
-    asteroid_list.sort()
+    asteroid_list.sort()                            #소팅
 
     while(len(asteroid_list) > 1):
-        a = asteroid_list.pop(len(asteroid_list) - 1)
+        a = asteroid_list.pop(len(asteroid_list) - 1)   #소팅한 배열의 큰값은 len()-1에 있고 빼낸다.
         b = asteroid_list.pop(len(asteroid_list) - 1)
-        if a == b:
+        if a == b:      #두 개의 무게가 같으면 빼낸채로 종료한다.
             continue
-        elif a > b:
+        elif a > b:     #a가 크면 빼서 남은 값을 다시 배열에 넣는다.
             result = a - b
             asteroid_list.append(result)
-        elif a < b:
+        elif a < b:     #이거 왜했지..?(안해도 된다. 왜냐하면 소팅하면 어차피 a가 큰 값이다.)
             result = b - a
             asteroid_list.append(result)
-        asteroid_list.sort()
+        asteroid_list.sort()    #새로운 값을 위한 소팅
 
-    if(len(asteroid_list) == 0):
+    if(len(asteroid_list) == 0):    #둘 다 사라져서 배열이 비어있으면 0을 추가한다.
         asteroid_list.append(0)
 
     return asteroid_list
 
 def main():
-    asteroid_list = list(map(int, input().split()))
+    asteroid_list = list(map(int, input().split()))     #소행성 정수
     
     print(*weigth(asteroid_list))
 
--- .vscode/Coding_Test/Coding_Test(6)/marathon.py
+++ .vscode/Coding_Test/Coding_Test(6)/marathon.py
@@ -1,12 +1,12 @@
 import collections
 
 def noncompletion(part, comp):
-    my_list = collections.Counter(part) - collections.Counter(comp)
+    my_list = collections.Counter(part) - collections.Counter(comp)     #key값을 비교해서 남은 key값이 my_list에 들어간다.
     return my_list
 
 def main():
-    participant = list(input().split())
-    completion = list(input().split())
+    participant = list(input().split())     #참가자 명단
+    completion = list(input().split())      #완주자 명단
     
     print(*noncompletion(participant, completion))
 

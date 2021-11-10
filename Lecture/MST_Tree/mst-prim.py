7c7a43c0b33d4401ab7df55e561541d2d562bd3f
DongYounYim <dyyim4725@gmail.com>
2021-11-11 00:48:42 +0900

    Add comment on Lecture

--- Lecture/MST_Tree/mst-prim.py
+++ Lecture/MST_Tree/mst-prim.py
@@ -2,10 +2,10 @@ import heapq
 
 def prim(edges, N):
     queue = []
-    heapq.heappush(queue, [0, 0, 1])
+    heapq.heappush(queue, [0, 0, 1])        #가상 가상 (첫 노드)
 
-    visit = [ False for _ in range(N+1)]
-    result = []
+    visit = [ False for _ in range(N+1)]        #방문한 노드
+    result = []         #edge정보 저장
     sum = 0
 
     while queue:
@@ -16,11 +16,11 @@ def prim(edges, N):
         else:
             visit[h[2]] = True
             sum += h[0]
-            result.append([h[1], h[2], h[0]])
+            result.append([h[1], h[2], h[0]])       #출력하기 위한 순서
 
-        for e in edges[h[2]]:
-            if (visit[e[0]]): continue
-            heapq.heappush(queue, [e[1], h[2], e[0]])
+        for e in edges[h[2]]:                       #갱신
+            if (visit[e[0]]): continue              #e[0]가 다음노드의 값
+            heapq.heappush(queue, [e[1], h[2], e[0]])   #e[1]에는 cost
     return sum, result
 
 def main():
@@ -31,8 +31,8 @@ def main():
 
     for _ in range(M):
         u, v, c = map(int, input().split())
-        edges[u].append([v, c])
-        edges[v].append([u, c])
+        edges[u].append([v, c])             #인접리스트
+        edges[v].append([u, c])             #무방향이라 반대까지
     print(prim(edges, N))
 
 if __name__ == "__main__":
